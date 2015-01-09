6824496f4ac29d03b9a4bd0ec7e7e806b72eca49
root <root@localhost>
2015-01-09 03:14:50 +0100

    Update docs

---
+++ target/doc/implementors/petgraph/trait.VisitMap.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors['petgraph'] = [];
+implementors['bin'] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
