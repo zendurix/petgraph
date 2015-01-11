994d8fc8e164c5c7cd195a2ad12a4c3346db822a
root <root@localhost>
2015-01-11 03:48:44 +0100

    Update docs

---
+++ target/doc/implementors/core/ops/trait.Index.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors['petgraph'] = ["<a class='stability Unstable' title='Unstable'></a>impl&lt;N, E, Ty&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Index.html' title='core::ops::Index'>Index</a>&lt;<a class='struct' href='petgraph/ograph/struct.NodeIndex.html' title='petgraph::ograph::NodeIndex'>NodeIndex</a>&gt; for <a class='struct' href='petgraph/ograph/struct.OGraph.html' title='petgraph::ograph::OGraph'>OGraph</a>&lt;N, E, Ty&gt;","<a class='stability Unstable' title='Unstable'></a>impl&lt;N, E, Ty&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Index.html' title='core::ops::Index'>Index</a>&lt;<a class='struct' href='petgraph/ograph/struct.EdgeIndex.html' title='petgraph::ograph::EdgeIndex'>EdgeIndex</a>&gt; for <a class='struct' href='petgraph/ograph/struct.OGraph.html' title='petgraph::ograph::OGraph'>OGraph</a>&lt;N, E, Ty&gt;",];
+implementors['bin'] = ["<a class='stability Unstable' title='Unstable'></a>impl&lt;N, E, Ty&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Index.html' title='core::ops::Index'>Index</a>&lt;<a class='struct' href='petgraph/ograph/struct.NodeIndex.html' title='petgraph::ograph::NodeIndex'>NodeIndex</a>&gt; for <a class='struct' href='petgraph/ograph/struct.OGraph.html' title='petgraph::ograph::OGraph'>OGraph</a>&lt;N, E, Ty&gt;","<a class='stability Unstable' title='Unstable'></a>impl&lt;N, E, Ty&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Index.html' title='core::ops::Index'>Index</a>&lt;<a class='struct' href='petgraph/ograph/struct.EdgeIndex.html' title='petgraph::ograph::EdgeIndex'>EdgeIndex</a>&gt; for <a class='struct' href='petgraph/ograph/struct.OGraph.html' title='petgraph::ograph::OGraph'>OGraph</a>&lt;N, E, Ty&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
