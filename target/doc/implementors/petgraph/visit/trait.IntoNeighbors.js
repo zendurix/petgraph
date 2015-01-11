e8e7cbd490b9b5fae9d5dd64b06da379c93d6810
root <root@localhost>
2015-01-11 20:41:22 +0100

    Update docs

--- target/doc/implementors/petgraph/visit/trait.IntoNeighbors.js
+++ target/doc/implementors/petgraph/visit/trait.IntoNeighbors.js
@@ -1,6 +1,5 @@
 (function() {var implementors = {};
 implementors['petgraph'] = [];
-implementors['bin'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
