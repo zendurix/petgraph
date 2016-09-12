1fab88cc066533510830cd334a180c72b110c70f
bluss <bluss@users.noreply.github.com>
2016-09-12 22:45:41 +0200

    Update docs

--- doc/implementors/core/ops/trait.Deref.js
+++ doc/implementors/core/ops/trait.Deref.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['petgraph'] = ["impl&lt;'b, T&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Deref.html' title='core::ops::Deref'>Deref</a> for <a class='struct' href='petgraph/graphmap/struct.Ptr.html' title='petgraph::graphmap::Ptr'>Ptr</a>&lt;'b, T&gt;",];
+implementors["petgraph"] = ["impl&lt;'b,&nbsp;T&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Deref.html' title='core::ops::Deref'>Deref</a> for <a class='struct' href='petgraph/graphmap/struct.Ptr.html' title='petgraph::graphmap::Ptr'>Ptr</a>&lt;'b,&nbsp;T&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- doc/implementors/petgraph/visit/trait.Revisitable.js
+++ doc/implementors/petgraph/visit/trait.Revisitable.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['petgraph'] = [];
+implementors["petgraph"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
